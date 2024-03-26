e79746e99f28d10548efe93fe0b5761cb3bd76db
LeeJunGyun <rainmaker0303@hanmail.net>
2024-03-26 21:55:49 +0900

    no message

--- src/main/kotlin/org/example/leetcode/Num1669.kt
+++ src/main/kotlin/org/example/leetcode/Num1669.kt
@@ -74,31 +74,29 @@ class Num1669MergeInBetweenLinkedLists {
 }
 
 
-class ListNode(var `val`: Int) {
-    var next: ListNode? = null
-
-    override fun toString(): String {
-        return "ListNode(`val`=$`val`, next=$next)"
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as ListNode
-
-        if (`val` != other.`val`) return false
-        return next == other.next
-    }
-
-    override fun hashCode(): Int {
-        var result = `val`
-        result = 31 * result + (next?.hashCode() ?: 0)
-        return result
-    }
-
-
-}
+//class ListNode(var `val`: Int) {
+//    var next: ListNode? = null
+//
+//    override fun toString(): String {
+//        return "ListNode(`val`=$`val`, next=$next)"
+//    }
+//
+//    override fun equals(other: Any?): Boolean {
+//        if (this === other) return true
+//        if (javaClass != other?.javaClass) return false
+//
+//        other as ListNode
+//
+//        if (`val` != other.`val`) return false
+//        return next == other.next
+//    }
+//
+//    override fun hashCode(): Int {
+//        var result = `val`
+//        result = 31 * result + (next?.hashCode() ?: 0)
+//        return result
+//    }
+//}
 
 fun main() {
 
