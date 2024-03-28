561f28a084091f0768c3d32b5f90391aabae8bb1
LeeJunGyun <rainmaker0303@hanmail.net>
2024-03-28 21:18:19 +0900

    Leetcode ListNode

--- src/main/kotlin/org/example/leetcode/ListNode.kt
+++ src/main/kotlin/org/example/leetcode/ListNode.kt
@@ -3,9 +3,9 @@ package org.example.leetcode
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
 
-    override fun toString(): String {
-        return "ListNode(`val`=$`val`, next=$next)"
-    }
+//    override fun toString(): String {
+//        return "ListNode(`val`=$`val`, next=$next)"
+//    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -22,4 +22,50 @@ class ListNode(var `val`: Int) {
         result = 31 * result + (next?.hashCode() ?: 0)
         return result
     }
+}
+
+fun getListNodeSize(head: ListNode?): Int {
+    var size = 1
+    var current = head
+    while (current?.next != null) {
+        current = current.next
+        size++
+    }
+    return size
+}
+
+fun addNode(head: ListNode?, `val`: Int): ListNode {
+    val newNode = ListNode(`val`)
+    if (head == null) {
+        return newNode
+    }
+    var current = head
+    while (current?.next != null) {
+        current = current.next
+    }
+    current?.next = newNode
+
+    return head
+}
+
+fun findNode(head: ListNode?, position: Int): ListNode? {
+    var current = head
+    var count = 0
+
+    while (current != null) {
+        if (count == position) {
+            return current
+        }
+        count++
+        current = current.next
+    }
+    return null
+}
+
+fun main() {
+    var head = ListNode(1)
+    addNode(head, 2)
+    addNode(head, 3)
+    println(head)
+    println(getListNodeSize(head))
 }
